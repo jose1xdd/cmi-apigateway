f3e1eb626d9f1f21a52012c42674477db03fb2ad
Jose David Castillo <jose1xdd@gmail.com>
2025-05-11 14:53:13 -0500

    ajustes

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ def read_root():
     return {"name": name}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))  # Usa 8000 si PORT no está definido 
+    port = int(os.getenv("PORT", 8080))  # Usa 8000 si PORT no está definido
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
