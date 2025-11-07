5ccfc79c902cbabf3341a2763d09f9fdeb6f41b6
Jose David Castillo <jose1xdd@gmail.com>
2025-11-07 00:19:49 -0500

    ajustes reuniones

--- app/services/asistencia_manager.py
+++ app/services/asistencia_manager.py
@@ -1,6 +1,5 @@
 import logging
 from typing import Any, Dict, Optional
-
 from app.client.ms_gestion_reuniones.asistencia.interface.interface_client_asistencia import IClientAsistencia
 
 
@@ -10,18 +9,15 @@ class AsistenciaManager:
         self.logger = logger
 
     def assign_asistencia(self, reunion_id: int, data: Dict[str, Any], headers):
-        self.logger.info(f"Asginando asistencia en la reunión {reunion_id}")
+        self.logger.info(f"[AsistenciaManager] Asignando asistencia en reunión {reunion_id}")
         return self.client_asistencia.assign_asistencia(reunion_id, data, headers)
 
     def user_assign_asistencia(self, reunion_id: int, data: Dict[str, Any], headers):
-        self.logger.info(
-            f"Asginando asistencia de usuario en la reunión {reunion_id}")
+        self.logger.info(f"[AsistenciaManager] Registrando asistencia (por documento) en reunión {reunion_id}")
         return self.client_asistencia.user_assign_asistencia(reunion_id, data, headers)
 
     def delete_asistencia(self, reunion_id: int, persona_id: int, headers):
-        self.logger.info(
-            f"Eliminando asistencia para persona_id={persona_id} en reunion_id={reunion_id}"
-        )
+        self.logger.info(f"[AsistenciaManager] Eliminando asistencia persona_id={persona_id}, reunión={reunion_id}")
         return self.client_asistencia.delete_asistencia(reunion_id, persona_id, headers)
 
     def get_personas_with_asistencia(
@@ -32,7 +28,7 @@ class AsistenciaManager:
         headers,
         numero_documento: Optional[str] = None,
         nombre: Optional[str] = None,
-        apellido: Optional[str] = None
+        apellido: Optional[str] = None,
     ):
         return self.client_asistencia.get_personas_with_asistencia(
             reunion_id=reunion_id,
@@ -41,7 +37,7 @@ class AsistenciaManager:
             headers=headers,
             numero_documento=numero_documento,
             nombre=nombre,
-            apellido=apellido
+            apellido=apellido,
         )
 
     def get_asistencia_persona(self, persona_id: int, reunion_id: int, headers):
