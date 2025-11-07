9a80fd2df2f5f697fcc2ccc6d694ef5a41651d7a
Jose David Castillo <jose1xdd@gmail.com>
2025-11-06 21:13:15 -0500

    refactorizacion

--- app/models/outputs/paginated_response.py
+++ app/models/outputs/paginated_response.py
@@ -2,9 +2,9 @@ from typing import List
 from pydantic import BaseModel
 
 from app.models.outputs.asistencia.asistencia_out import PersonaAsistenciaOut
-from app.models.outputs.familia.familia_output import FamiliaOut
+from app.models.outputs.familia.familia_output import FamiliaDataLeader, FamiliaOut
 from app.models.outputs.parcialidad.parcialidad_output import ParcialidadOut
-from app.models.outputs.persona.persona_output import PersonaOut
+from app.models.outputs.persona.persona_output import PersonaFamiliaOut, PersonaOut
 from app.models.outputs.publicacion.publicacion_out import PublicacionOut
 from app.models.outputs.reunion.reunion_out import ReunionOut
 from app.models.outputs.usuario.usuario_output import UsuarioOut
@@ -55,8 +55,23 @@ class PaginatedReunion(BaseModel):
     total_pages: int
     items: List[ReunionOut]
 
-class PaginatedAsistenciaPersonas(BaseModel):
+class PaginatedDataLeader(BaseModel):
     total_items: int
     current_page: int
     total_pages: int
-    items: List[PersonaAsistenciaOut]
+    items: List[FamiliaDataLeader]
+
+class PaginatedParcialidad(BaseModel):
+    total_items: int
+    current_page: int
+    total_pages: int
+    items: List[ParcialidadOut]
+
+class PaginatedPersonasFamilia(BaseModel):
+    total_items: int
+    total_pages: int
+    current_page: int
+    items: List[PersonaFamiliaOut]
+
+    class Config:
+        from_attributes = True
